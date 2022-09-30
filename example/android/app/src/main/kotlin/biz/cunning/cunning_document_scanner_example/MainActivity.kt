0ead51c83d23f951c0f302dd77c5f84fcdf0f3cb
jachzen <pater.marcel@gmail.com>
2022-09-30 15:32:20 +0200

    Fixed warnings

--- example/android/app/src/main/kotlin/biz/cunning/cunning_document_scanner_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/biz/cunning/cunning_document_scanner_example/MainActivity.kt
@@ -2,5 +2,4 @@ package biz.cunning.cunning_document_scanner_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
