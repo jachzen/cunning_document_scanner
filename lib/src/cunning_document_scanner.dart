6fb7c66abdef87fd5c243593e3434ea474a53400
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-12-03 14:40:58 +0100

    chore: Remove permission_handler dependency
    The responsibility for handling camera permissions is delegated to the user of the library. This change removes the `permission_handler` package and the associated permission request logic from the `getPictures` method.

--- lib/src/cunning_document_scanner.dart
+++ lib/src/cunning_document_scanner.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-import 'exceptions.dart';
 import 'ios_scanner_options.dart';
 
 /// A class that provides a simple way to scan documents.
@@ -26,15 +24,6 @@ class CunningDocumentScanner {
     bool isGalleryImportAllowed = false,
     IosScannerOptions? iosScannerOptions,
   }) async {
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.camera,
-    ].request();
-    if (statuses.containsValue(PermissionStatus.denied) ||
-        statuses.containsValue(PermissionStatus.permanentlyDenied)) {
-      throw const CunningDocumentScannerException.permissionDenied(
-          'Camera permission not granted');
-    }
-
     final List<dynamic>? pictures = await _channel.invokeMethod('getPictures', {
       'noOfPages': noOfPages,
       'isGalleryImportAllowed': isGalleryImportAllowed,
